8955e68d01da560468d37a769c582c26b183061e
Richard Allwood <richard.m.allwood@gmail.com>
2016-07-30 20:51:25 +1000

    Added heatmaps

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
+var heatmap = null;
 
+function onShowHideCarDensity() {
+    console.log("SHOW/HIDE");
+    if (heatmap == null) {
+        $.get(apiUrl + "api/monitoring/Air%20quality", function(data) {
+            var heatMapData = [ ];
+
+            $.each(data, function(index) {
+                var f = data[index];
+                heatMapData.push({ location: new google.maps.LatLng(
+                        parseFloat(f.Latitude),
+                        parseFloat(f.Longitude)),
+                    weight: 5
+                });
+            });
+            heatmap = new google.maps.visualization.HeatmapLayer({ data: heatMapData, map: window.map });
+        });
+    } else {
+        heatmap.setMap(null);
+        heatmap = null;
+    }
+}
 
