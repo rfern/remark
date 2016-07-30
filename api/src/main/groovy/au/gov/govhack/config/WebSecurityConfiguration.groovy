6202a7e110c54a10608de6cbe9631ba0e821ef08
Richard Allwood <richard.m.allwood@gmail.com>
2016-07-30 16:43:48 +1000

    Added cors filter to the API

--- api/src/main/groovy/au/gov/govhack/config/WebSecurityConfiguration.groovy
+++ api/src/main/groovy/au/gov/govhack/config/WebSecurityConfiguration.groovy
@@ -1,4 +1,4 @@
-package au.gov.govhack
+package au.gov.govhack.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
